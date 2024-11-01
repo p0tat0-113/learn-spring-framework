a889af9be7b3dd84c077efe45ef7db94029994ed
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-01 18:41:35 +0900

    MarioGame에 조작 기능 추가. 벌써부터 GameRunner가 MarioGame과 '강한결합'을 하면서 나타날 문제들이 보인다.

--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -9,5 +9,11 @@ public class GameRunner {
 
     public void run(){
         System.out.println("Running game: " + game);//이럴 때는 보통 로깅 프레임워크를 사용하는 것이 좋다고 함.
+
+        //MarioGame의 메서드를 호출
+        game.up();
+        game.down();
+        game.left();
+        game.right();
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
@@ -1,4 +1,20 @@
 package com.in28minutes.learn_spring_framework.game;
 
 public class MarioGame {
+    //기본적인 조작기능 구현
+    public void up(){
+        System.out.println("Jump");
+    }
+
+    public void down(){
+        System.out.println("Go into a hole");
+    }
+
+    public void left(){
+        System.out.println("Go back");
+    }
+
+    public void right(){
+        System.out.println("Accelerate");
+    }
 }
