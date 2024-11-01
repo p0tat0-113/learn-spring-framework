b8b1c0f1ac689b00dd8d273eec5fc358cebb7df5
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-01 18:48:44 +0900

    SuperContraGame 클래스 생성 SuperContraGame을 인수로 넣으면 컴파일 에러가 발생한다. GameRunner는 MarioGame에 의존하고 있기 때문.

---
+++ src/main/java/com/in28minutes/learn_spring_framework/game/SuperContraGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class SuperContraGame {
+    //기본적인 조작기능 구현. SupertMario와 같은 메서드이지만 내부 구현이 다르다.
+    public void up(){
+        System.out.println("Up");
+    }
+
+    public void down(){
+        System.out.println("Sit Down");
+    }
+
+    public void left(){
+        System.out.println("Go back");
+    }
+
+    public void right(){
+        System.out.println("Shoot a bullet");
+    }
+}
