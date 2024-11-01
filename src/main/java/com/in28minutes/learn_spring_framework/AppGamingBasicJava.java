862c58ddd60b6b2be21c5a731049b8aad21bf43f
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-01 19:07:33 +0900

    이제 스프링 프레임워크가 나 대신 인스턴스를 생성하고, 의존성을 주입하게 할 것이다.

--- src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -7,11 +7,13 @@ import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
+
+        //1. 인스턴스 생성
         MarioGame marioGame = new MarioGame();
         SuperContraGame superContra = new SuperContraGame();
         PacmanGame pacManGame = new PacmanGame();
 
-        GameRunner gameRunner = new GameRunner(pacManGame);//이제 게임을 바꿀 때 GameRunner의 코드를 단 하나도 바꿀 필요 없다. "느슨한 결합"
+        GameRunner gameRunner = new GameRunner(pacManGame);//2. 인스턴스 생성 + 의존성 주입
         gameRunner.run();
     }
 }
