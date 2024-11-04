41970a580c46c3375ad46530ab1c1c0eee301e53
gamja3 <dwkimdwkim1005@gmail.com>
2024-11-04 12:55:26 +0900

    이번에는 GameRunner까지 @Component애노테이션을 붙여서, 설정파일에서 모든 @Bean메서드를 제거했다.
    스프링은 스스로 올바른 패키지를 스캔하여 컴포넌트들을 찾고, 인스턴스를 생성하고, 자동으로 의존성을 주입해준다. 많은 것을 스프링에게 맞길 수 있게 되었음. 코드가 많이 단순해졌다.

--- src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
+@ComponentScan은 스프링이 특정 패키지 내에서 @Component와 그 파생 어노테이션이 붙은 클래스를 자동으로 감지하여 빈으로 등록하도록 지시하는 애노테이션입니다.
+이를 통해 개발자는 수동으로 빈을 등록하지 않고도 필요한 클래스들을 빈으로 관리할 수 있습니다.
+*/
+
 @Configuration
 @ComponentScan("package com.in28minutes.learn_spring_framework.game")//Configuration파일에 컴포넌트를 어디에서 스캔해야할지 알려준다.
 public class GamingAppLauncherApplication {
@@ -16,4 +21,19 @@ public class GamingAppLauncherApplication {
             context.getBean(GameRunner.class).run();//스프링 빈으로 등록된 GameRunner를 꺼내와서 run()메서드를 호출한다. 스프링이 Configuration파일을 읽고 의존성을 주입해줬기 때문에 게임이 실행된다.
         }
     }
+
+    /*
+    동작 방식
+    설정 클래스에 @ComponentScan 선언:
+    스프링은 설정 클래스(@Configuration이 붙은 클래스)에 선언된 @ComponentScan을 통해 스캔할 패키지를 파악합니다.
+
+    패키지 내 클래스 검색:
+    지정된 패키지와 그 하위 패키지에서 @Component와 그 파생 어노테이션이 붙은 클래스를 검색합니다.
+
+    빈으로 등록:
+    검색된 클래스는 스프링 컨테이너에 빈으로 등록됩니다. 빈의 이름은 기본적으로 클래스 이름의 첫 글자를 소문자로 변환한 형태(myComponent)이지만, @Component("customName")처럼 명시적으로 지정할 수도 있습니다.
+
+    의존성 주입:
+    등록된 빈들은 다른 빈들 간의 의존성을 주입받을 때 사용됩니다. 예를 들어, @Autowired를 통해 자동으로 주입됩니다.
+    */
 }
--- src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
@@ -2,6 +2,12 @@ package com.in28minutes.learn_spring_framework.game;
 
 import org.springframework.stereotype.Component;
 
+/*
+일반적인 스테레오타입: @Component는 특정한 역할을 나타내지 않는 일반적인 빈을 정의할 때 사용됩니다.
+스테레오타입 어노테이션의 기반: @Service, @Repository, @Controller 등 다른 스테레오타입 어노테이션들은 모두 @Component를 메타 애노테이션으로 사용합니다. 이는 특정 역할을 나타내지만, 기본적으로는 @Component의 기능을 상속받습니다.
+자동 감지: @Component가 붙은 클래스는 @ComponentScan에 의해 자동으로 감지되고 빈으로 등록됩니다.
+*/
+
 //Configuration파일에서 @Bean으로 스프링 빈을 정의하지 않고,
 //이렇게 @Component 애노테이션을 붙여서 스프링에게 이 클래스의 인스턴스 생성을 요청할 수 있다.
 @Component
